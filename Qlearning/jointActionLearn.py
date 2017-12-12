3bda88cc8793062af6d30501d432a06c574ecdc5
IrisGuo <yanfguo@vub.ac.be>
2017-12-12 10:58:30 +0100

    mark point4

--- Qlearning/jointActionLearn.py
+++ Qlearning/jointActionLearn.py
@@ -8,6 +8,7 @@ class JointActionLearn():
         self._musigma=[[[11,sigma0],[-30,sigma],[0,sigma]],[[-30,sigma],[7,sigma1],[6,sigma]],[[0,sigma],[0,sigma],[5,sigma]]]
         self.q = []
         self.qStar=[[[],[],[]],[[],[],[]],[[],[],[]]]
+
         self.step = 0
         self.probA = np.zeros(3)+1/3
         self.probB = np.zeros(3)+1/3
@@ -128,7 +129,7 @@ class JointActionLearn():
             reward = []
             for j in range(3):
                 if len(self.qStar[i][j])>0:
-                    reward.append(np.mean(self.qStar[i][j]))
+                    reward.append(np.max(self.qStar[i][j]))
                 else:
                     reward.append(0)
 
@@ -232,20 +233,181 @@ class JointActionLearn():
 
 
 a = JointActionLearn(0.2,0.2,0.2)
-a.playOptiBolt()
-print(a.probA)
-print(a.probB)
-for i in range(3):
-    for j in range(3):
-        if len(a.qStar[i][j])>0:
-            print(np.mean(a.qStar[i][j]))
-        else:
-            print(0)
-        print(len(a.qStar[i][j]))
-print(np.mean(a.q))
+a1 = JointActionLearn(0.2,0.2,0.2)
+b = JointActionLearn(4,0.1,0.1)
+b1= JointActionLearn(4,0.1,0.1)
+c = JointActionLearn(0.1,0.1,4)
+c1= JointActionLearn(0.1,0.1,4)
+
+def movAvg(alist,window):
+    # use an odd integer
+    step = int((window-1)/2)
+    start = step
+    end = len(alist)-step
+    res = []
+    for i in range(0,start):
+        res.append(np.mean(alist[0:i+1]))
+    for i in range(start,end):
+        res.append(np.mean(alist[i-step:i+step+1]))
+    for i in range(end,len(alist)-end):
+        res.append(alist[i])
+    return res
+
+
+a.playBolt()
+a1.playOptiBolt()
+b.playBolt()
+b1.playOptiBolt()
+c.playBolt()
+c1.playOptiBolt()
+
+aq = movAvg(a.q,51)
+a1q = movAvg(a1.q,51)
+
+bq = movAvg(b.q,51)
+b1q = movAvg(b1.q,51)
+cq = movAvg(c.q,51)
+c1q = movAvg(c1.q,51)
 
 plt.figure()
-t = range(len(a.probAList[0]))
-for i in range(3):
-    plt.plot(t,a.probAList[i])
-plt.show()
+plt.plot(aq)
+plt.plot(a1q)
+plt.title("Reward(window=51) sigma0=sigma1=sigma= 0.2")
+plt.xlabel("step")
+plt.ylabel("reward")
+plt.legend(["simple Boltzmann","(optimistic boltzmann"])
+plt.savefig("./JAL/jalr1.png")
+
+plt.figure()
+plt.plot(bq)
+plt.plot(b1q)
+plt.title("Reward(window=51) sigma0=4,sigma1=sigma= 0.1")
+plt.xlabel("step")
+plt.ylabel("reward")
+plt.legend(["simple Boltzmann","(optimistic boltzmann"])
+plt.savefig("./JAL/jalr2.png")
+
+plt.figure()
+plt.plot(cq)
+plt.plot(c1q)
+plt.title("Reward(window=51) sigma1=4,sigma0=sigma= 0.1")
+plt.xlabel("step")
+plt.ylabel("reward")
+plt.legend(["simple Boltzmann","(optimistic boltzmann"])
+plt.savefig("./JAL/jalr3.png")
+
+plt.figure(figsize=(15,20))
+plt.subplot(3,2,1)
+plt.plot(a.probAList[0])
+plt.plot(a.probAList[1])
+plt.plot(a.probAList[2])
+plt.xlabel("step")
+plt.ylabel("Probability")
+plt.legend(['A-action0','A-action1','A-action2'])
+plt.title("Probability of A's actions\n(sigma0=sigma1=sigma2=0.2,optimistic boltzmann)")
+
+plt.subplot(3,2,2)
+plt.plot(a.probBList[0])
+plt.plot(a.probBList[1])
+plt.plot(a.probBList[2])
+plt.xlabel("step")
+plt.ylabel("Probability")
+plt.legend(['B-action0','B-action1','B-action2'])
+plt.title("Probability of B's actions\n(sigma0=sigma1=sigma2=0.2,optimistic boltzmann)")
+
+
+plt.subplot(3,2,3)
+plt.plot(b.probAList[0])
+plt.plot(b.probAList[1])
+plt.plot(b.probAList[2])
+plt.xlabel("step")
+plt.ylabel("Probability")
+plt.legend(['A-action0','A-action1','A-action2'])
+plt.title("Probability of A's actions\n(sigma0=4,sigma1=sigma= 0.1,optimistic boltzmann)")
+
+plt.subplot(3,2,4)
+plt.plot(b.probBList[0])
+plt.plot(b.probBList[1])
+plt.plot(b.probBList[2])
+plt.xlabel("step")
+plt.ylabel("Probability")
+plt.legend(['B-action0','B-action1','B-action2'])
+plt.title("Probability of B's actions\n(sigma0=4,sigma1=sigma= 0.1,optimistic boltzmann)")
+
+plt.subplot(3,2,5)
+plt.plot(c.probAList[0])
+plt.plot(c.probAList[1])
+plt.plot(c.probAList[2])
+plt.xlabel("step")
+plt.ylabel("Probability")
+plt.legend(['A-action0','A-action1','A-action2'])
+plt.title("Probability of A's actions\n(sigma1=4,sigma0=sigma= 0.1,optimistic boltzmann)")
+
+plt.subplot(3,2,6)
+plt.plot(c.probBList[0])
+plt.plot(c.probBList[1])
+plt.plot(c.probBList[2])
+plt.xlabel("step")
+plt.ylabel("Probability")
+plt.legend(['B-action0','B-action1','B-action2'])
+plt.title("Probability of B's actions\n(sigma1=4,sigma0=sigma= 0.1,optimistic boltzmann)")
+
+plt.savefig("./JAL/prob1.png")
+
+plt.figure(figsize=(15,20))
+plt.subplot(3,2,1)
+plt.plot(a1.probAList[0])
+plt.plot(a1.probAList[1])
+plt.plot(a1.probAList[2])
+plt.xlabel("step")
+plt.ylabel("Probability")
+plt.legend(['A-action0','A-action1','A-action2'])
+plt.title("Probability of A's actions\n(sigma0=sigma1=sigma2=0.2,optimistic boltzmann)")
+
+plt.subplot(3,2,2)
+plt.plot(a1.probBList[0])
+plt.plot(a1.probBList[1])
+plt.plot(a1.probBList[2])
+plt.xlabel("step")
+plt.ylabel("Probability")
+plt.legend(['B-action0','B-action1','B-action2'])
+plt.title("Probability of B's actions\n(sigma0=sigma1=sigma2=0.2,optimistic boltzmann)")
+
+
+plt.subplot(3,2,3)
+plt.plot(b1.probAList[0])
+plt.plot(b1.probAList[1])
+plt.plot(b1.probAList[2])
+plt.xlabel("step")
+plt.ylabel("Probability")
+plt.legend(['A-action0','A-action1','A-action2'])
+plt.title("Probability of A's actions\n(sigma0=4,sigma1=sigma= 0.1,optimistic boltzmann)")
+
+plt.subplot(3,2,4)
+plt.plot(b1.probBList[0])
+plt.plot(b1.probBList[1])
+plt.plot(b1.probBList[2])
+plt.xlabel("step")
+plt.ylabel("Probability")
+plt.legend(['B-action0','B-action1','B-action2'])
+plt.title("Probability of B's actions\n(sigma0=4,sigma1=sigma= 0.1,optimistic boltzmann)")
+
+plt.subplot(3,2,5)
+plt.plot(c1.probAList[0])
+plt.plot(c1.probAList[1])
+plt.plot(c1.probAList[2])
+plt.xlabel("step")
+plt.ylabel("Probability")
+plt.legend(['A-action0','A-action1','A-action2'])
+plt.title("Probability of A's actions\n(sigma1=4,sigma0=sigma= 0.1,optimistic boltzmann)")
+
+plt.subplot(3,2,6)
+plt.plot(c.probBList[0])
+plt.plot(c.probBList[1])
+plt.plot(c.probBList[2])
+plt.xlabel("step")
+plt.ylabel("Probability")
+plt.legend(['B-action0','B-action1','B-action2'])
+plt.title("Probability of B's actions\n(sigma1=4,sigma0=sigma= 0.1,optimistic boltzmann)")
+
+plt.savefig("./JAL/prob2.png")
--- Qlearning/nArmQlearning.py
+++ Qlearning/nArmQlearning.py
@@ -3,6 +3,8 @@ import math
 
 import matplotlib.pyplot as plt
 
+trials = 10
+
 def meanList(list):
     if len(list)==0:
         return 0
@@ -10,6 +12,7 @@ def meanList(list):
         return np.mean(list)
 
 reward1 = [{'mu': 2.1, 'sigma': 0.9}, {'mu': 1.1, 'sigma': 0.8}, {'mu': 0.7, 'sigma': 2}, {'mu': 1.9, 'sigma': 0.9}]
+reward2 = [{'mu': 2.1, 'sigma': 0.9*2}, {'mu': 1.1, 'sigma': 0.8*2}, {'mu': 0.7, 'sigma': 2*2}, {'mu': 1.9, 'sigma': 0.9*2}]
 
 
 class NArmQlearning(object):
@@ -18,11 +21,12 @@ class NArmQlearning(object):
         # there are 4 actions
         self._musigma = musigma
         # intialize the Q value
-        self.qstarSum = [0,0,0,0]
-        self.qStar = [[], [], [], []]
+        self.qStarSum = [0,0,0,0]
+        self.actionTimes=[0,0,0,0]
+        self.qStarAvg = [[0], [0], [0], [0]]
         # the actions selected in each run
         self.rewardSum = 0
-        self.reward = []
+        self.rewardAvg = []
         self.action = []
         self.step = 0
 
@@ -31,47 +35,39 @@ class NArmQlearning(object):
         self.step += 1
         reward_action = []
         for i in range(4):
-            if len(self.qStar[i]) > 0:
-                reward_action.append(np.mean(self.qStar[i]))
-            else:
-                reward_action.append(0)
+            reward_action.append(self.qStarAvg[i][-1])
 
         maxIndex = np.argmax(reward_action)
-        maxReward = np.random.normal(self._musigma[maxIndex]['mu'], self._musigma[maxIndex]['sigma'])
 
         randomAction = np.random.randint(0, 4)
-        randomReward = np.random.normal(self._musigma[randomAction]['mu'], self._musigma[randomAction]['sigma'])
 
         randomNum = np.random.uniform(0, 1)
         if randomNum < epsilon:
-            # if smaller than episilon, random select an action
-            self.rewardSum +=randomReward
-            self.reward.append(self.rewardSum/self.step)
-            self.action.append(randomAction)
-            for i in range(4):
-                if i != randomAction:
-                    self.qStar[i].append(self.re)
-                else:
-
-                    self.qStar[randomAction].append(randomReward)
+            actionChosen = randomAction
         else:
-            self.reward.append(maxReward)
-            self.action.append(maxIndex)
-            for i in range(4):
-                if i != maxIndex:
-                    self.qStar[i].append(reward_action[i])
-                else:
+            actionChosen = maxIndex
 
-                    self.qStar[maxIndex].append(maxReward)
+        # update the q/action/qstar/sum
+        self.action.append(actionChosen)
+        self.actionTimes[actionChosen] +=1
+        reward = np.random.normal(self._musigma[actionChosen]['mu'], self._musigma[actionChosen]['sigma'])
+        self.rewardSum +=  reward
+        self.rewardAvg.append(self.rewardSum/self.step)
+
+        self.qStarSum[actionChosen] += reward
+        for i in range(4):
+            if i == actionChosen:
+                self.qStarAvg[i].append(self.qStarSum[i]/self.actionTimes[i])
+            else:
+                self.qStarAvg[i].append(self.qStarAvg[i][-1])
+
+
 
     def _playtau(self, tau):
         self.step += 1
         reward_action = []
         for i in range(4):
-            if len(self.qStar[i]) > 0:
-                reward_action.append(np.mean(self.qStar[i]))
-            else:
-                reward_action.append(0)
+            reward_action.append(self.qStarAvg[i][-1])
 
         eqt = []
         for i in range(4):
@@ -82,63 +78,54 @@ class NArmQlearning(object):
 
         # choosing action
         randomNum = np.random.uniform(0, 1)
-        if randomNum < prob[0]:
-            # choosing action 0
-            self.action.append(0)
-            reward = np.random.normal(self._musigma[0]['mu'], self._musigma[0]['sigma'])
-            self.reward.append(reward)
-            self.qStar[0].append(reward)
-            for i in [1, 2, 3]:
-                self.qStar[i].append(reward_action[i])
-        elif randomNum < sum(prob[0:2]):
-            # choosing action 1
-            self.action.append(1)
-            reward = np.random.normal(self._musigma[1]['mu'], self._musigma[1]['sigma'])
-            self.reward.append(reward)
-            self.qStar[1].append(reward)
-            for i in [0, 2, 3]:
-                self.qStar[i].append(reward_action[i])
-        elif randomNum < sum(prob[0:3]):
-            # choosing action 3
-            self.action.append(2)
-            reward = np.random.normal(self._musigma[2]['mu'], self._musigma[2]['sigma'])
-            self.reward.append(reward)
-            self.qStar[2].append(reward)
-            for i in [0, 1, 3]:
-                self.qStar[i].append(reward_action[i])
+        if randomNum<prob[0]:
+            actionChosen = 0
+        elif randomNum < sum(prob[:2]):
+            actionChosen =1
+        elif randomNum <sum(prob[:3]):
+            actionChosen = 2
         else:
-            self.action.append(3)
-            reward = np.random.normal(self._musigma[3]['mu'], self._musigma[3]['sigma'])
-            self.reward.append(reward)
-            self.qStar[3].append(reward)
-            for i in [0, 1, 2]:
-                self.qStar[i].append(reward_action[i])
+            actionChosen = 3
 
+        self.action.append(actionChosen)
+        self.actionTimes[actionChosen] += 1
+        reward = np.random.normal(self._musigma[actionChosen]['mu'], self._musigma[actionChosen]['sigma'])
+        self.rewardSum += reward
+        self.rewardAvg.append(self.rewardSum / self.step)
+
+        self.qStarSum[actionChosen] += reward
+        for i in range(4):
+            if i == actionChosen:
+                self.qStarAvg[i].append(self.qStarSum[i] / self.actionTimes[i])
+            else:
+                self.qStarAvg[i].append(self.qStarAvg[i][-1])
 
     def _playrandom(self):
         self.step += 1
 
         randomNum = np.random.uniform(0,1)
         if randomNum<0.25:
-            actionchosen = 0
+            actionChosen = 0
         elif randomNum <0.5:
-            actionchosen = 1
+            actionChosen = 1
         elif randomNum <0.75:
-            actionchosen = 2
+            actionChosen = 2
         else:
-            actionchosen = 3
+            actionChosen = 3
 
-        reward = np.random.normal(self._musigma[actionchosen]['mu'], self._musigma[actionchosen]['sigma'])
 
-        # update the q/qstar/action
-        self.action.append(actionchosen)
-        self.reward.append(reward)
+        self.action.append(actionChosen)
+        self.actionTimes[actionChosen] += 1
+        reward = np.random.normal(self._musigma[actionChosen]['mu'], self._musigma[actionChosen]['sigma'])
+        self.rewardSum += reward
+        self.rewardAvg.append(self.rewardSum / self.step)
+
+        self.qStarSum[actionChosen] += reward
         for i in range(4):
-            if i == actionchosen:
-                self.qStar[i].append(reward)
+            if i == actionChosen:
+                self.qStarAvg[i].append(self.qStarSum[i] / self.actionTimes[i])
             else:
-                self.qStar[i].append(meanList(self.qStar[i]))
-
+                self.qStarAvg[i].append(self.qStarAvg[i][-1])
 
 
 
@@ -154,7 +141,7 @@ class NArmQlearning(object):
         for i in range(1000):
             self._play_epsilon(0.1)
 
-    def playepisilonT(self):
+    def playepsilonT(self):
         for i in range(1000):
             epi = 1/np.sqrt(i+1)
             self._play_epsilon(epi)
@@ -180,137 +167,336 @@ class NArmQlearning(object):
 
 
 def draw_q(list):
-    a = []
     t = range(len(list))
-    for i in t:
-        a.append(np.mean(list[:i + 1]))
-
     plt.ylim([0, 3])
-    plt.plot(t, a)
+    plt.plot(t, list)
 
 
-'''
-a = NArmQlearning(reward1)
-a.playepsilon1()
-print(np.mean(a.reward))
-plt.figure()
-draw_q(a.reward)
-draw_q(a.qStar[0])
-draw_q(a.qStar[1])
-draw_q(a.qStar[2])
-draw_q(a.qStar[3])
-plt.legend(['q','qStar1','qStar2','qStar3','qStar4'])
-plt.show()
-
-'''
-'''
-a = NArmQlearning(reward1)
-b = NArmQlearning(reward1)
-c = NArmQlearning(reward1)
-d = NArmQlearning(reward1)
-e = NArmQlearning(reward1)
-
-a.playepsilon0()
-b.playepsilon1()
-c.playepsilon2()
-d.playtau1()
-e.playtau2()
-
-
-
-plt.figure()
-draw_q(a.reward)
-draw_q(b.reward)
-draw_q(c.reward)
-draw_q(d.reward)
-draw_q(e.reward)
-
-plt.legend(['epsilon=0','epsilon=0.01','epsilon=0.1','tau=1','tau=0.01'])
-plt.title("Reward for 5 algorithms")
-plt.xlabel("Step")
-plt.ylabel("Reward")
-plt.savefig('./fig/reward.png')
-
-
-
-for i in range(4):
-    plt.figure()
-    draw_q(a.qStar[i])
-    draw_q(b.qStar[i])
-    draw_q(c.qStar[i])
-    draw_q(d.qStar[i])
-    draw_q(e.qStar[i])
-
-
-    plt.legend(['epsilon=0','epsilon=0.01','epsilon=0.1','tau=1','tau=0.01'])
-    plt.title("Reward for action %d  of 5 algorithms" %i)
-    plt.xlabel("Step")
-    plt.ylabel("Reward")
-
-    plt.savefig("./fig/action%d.png"%i)
-
-
-
-plt.figure()
-plt.hist(a.action)
-plt.title("histogram for action chosen")
-plt.savefig("./fig/hista.png")
-plt.figure()
-plt.hist(b.action)
-plt.title("histogram for action chosen")
-plt.savefig("./fig/histb.png")
-plt.figure()
-plt.hist(c.action)
-plt.title("histogram for action chosen")
-plt.savefig("./fig/histc.png")
-plt.figure()
-plt.hist(d.action)
-plt.title("histogram for action chosen")
-plt.savefig("./fig/histd.png")
-plt.figure()
-plt.hist(e.action)
-plt.title("histogram for action chosen")
-plt.savefig("./fig/histe.png")
-
-'''
-# play multiple times
-
-a=[]
-
-for i in range(10):
-    temp = []
-    for j in range(6):
-        temp.append(NArmQlearning(reward1))
-    a.append(temp)
-
-for i in range(10):
-    a[i][0].playepsilon0()
-    a[i][1].playepsilon1()
-    a[i][2].playepsilon2()
-    a[i][3].playtau1()
-    a[i][4].playtau2()
-    a[i][5].playrandom()
-
-# draw the mean value
-playerMeanReward = []
+agents=[]
 for i in range(6):
     temp = []
-    for j in range(10):
-        temp.append(a[j][i].reward)
+    #play 10 times
+    for j in  range(trials):
+        temp.append(NArmQlearning(reward1))
+    agents.append(temp)
 
-    bb = []
-    for i in range(1000):
-        tempList = [temp[j][i] for j in range(6)]
-        tempmean = meanList(tempList)
-        bb.append(tempmean)
-    playerMeanReward.append(bb)
+# play 10 times and then avg the results
+for i in range(10):
+    agents[0][i].playepsilon0()
+    agents[1][i].playepsilon1()
+    agents[2][i].playepsilon2()
+    agents[3][i].playtau1()
+    agents[4][i].playtau2()
+    agents[5][i].playrandom()
+
+# avg the results
+rewardAvg = []
+rewardStd = []
+for i in range(6):
+    tavg=[]
+    tstd=[]
+    for j in range(1000):
+        t = [agents[i][k].rewardAvg[j] for k in range(trials)]
+        avg = np.mean(t)
+        tavg.append(avg)
+        tstd.append(np.std(t))
+
+    rewardAvg.append(tavg)
+    rewardStd.append(tstd)
+
 
 plt.figure()
-for i in range(1):
-    t = range(len(playerMeanReward[i]))
-    plt.plot(t,playerMeanReward[i])
-    plt.ylim([0,3])
 
-plt.show()
+for i in range(6):
+    draw_q(rewardAvg[i])
 
+
+plt.legend(["epsilon0",'epsilon0.01','episilon0.1','tau1','tau0.1','random'])
+plt.title("average reward over time for 6 algorithms(run each algorithm %d times)"%trials)
+plt.xlabel("step")
+plt.ylabel("reward")
+plt.savefig("./fig/reward_all.png")
+
+plt.figure()
+for i in range(6):
+    draw_q(rewardStd[i])
+
+plt.legend(["epsilon0", 'epsilon0.01', 'episilon0.1', 'tau1', 'tau0.1', 'random'])
+plt.title("reward std over time for 6 algorithms(run each algorithm %d times)" % trials)
+plt.xlabel("step")
+plt.ylabel("reward")
+plt.savefig("./fig/reawrd_std.png")
+
+
+qstarAvg = []
+qstarStd = []
+
+for i in range(6):
+    agAvg = []
+    agStd = []
+    for action in range(4):
+        tAvg = []
+        tStd = []
+        for step in range(1000):
+            t = [agents[i][k].qStarAvg[action][step] for k in range(10)]
+            tAvg.append(np.mean(t))
+            tStd.append(np.std(t))
+        agAvg.append(tAvg)
+        agStd.append(tStd)
+    qstarAvg.append(agAvg)
+    qstarStd.append(agStd)
+
+plt.figure(figsize=(20,20))
+for i in range(4):
+    plt.subplot(2,2,i+1)
+    for j in range(6):
+        draw_q(qstarAvg[j][i])
+    plt.title("the average reward of action %d" %i)
+    plt.xlabel("step")
+    plt.ylabel("reward")
+    plt.legend(["epsilon0", 'epsilon0.01', 'episilon0.1', 'tau1', 'tau0.1', 'random'])
+plt.savefig("./fig/actionAll.png")
+
+plt.figure(figsize=(20,20))
+for i in range(4):
+    plt.subplot(2,2,i+1)
+    for j in range(6):
+        draw_q(qstarStd[j][i])
+    plt.title("the average std of action %d" %i)
+    plt.xlabel("step")
+    plt.ylabel("reward")
+    plt.legend(["epsilon0", 'epsilon0.01', 'episilon0.1', 'tau1', 'tau0.1', 'random'])
+plt.savefig("./fig/actionStd.png")
+
+plt.figure(figsize=(15,20))
+title= ['(epsilon=0)','(epsilon=0.01)','(epsilon=0.1)','(tau=1)','(tau=0.1)','(random)']
+for i in range(6):
+    plt.subplot(3,2,i+1)
+    action = [0 for i in range(1000*trials)]
+    for j in range(trials):
+        action[j*1000:(j+1)*1000]= agents[i][j].action[:]
+    plt.hist(action)
+    plt.title("histogram of actions selection (%d) trials "%trials+title[i])
+plt.savefig("./fig/hist.png")
+
+
+# for double std
+agents=[]
+for i in range(6):
+    temp = []
+    #play 10 times
+    for j in  range(trials):
+        temp.append(NArmQlearning(reward2))
+    agents.append(temp)
+
+# play 10 times and then avg the results
+for i in range(10):
+    agents[0][i].playepsilon0()
+    agents[1][i].playepsilon1()
+    agents[2][i].playepsilon2()
+    agents[3][i].playtau1()
+    agents[4][i].playtau2()
+    agents[5][i].playrandom()
+
+# avg the results
+rewardAvg = []
+rewardStd = []
+for i in range(6):
+    tavg=[]
+    tstd=[]
+    for j in range(1000):
+        t = [agents[i][k].rewardAvg[j] for k in range(trials)]
+        avg = np.mean(t)
+        tavg.append(avg)
+        tstd.append(np.std(t))
+
+    rewardAvg.append(tavg)
+    rewardStd.append(tstd)
+
+
+plt.figure()
+
+for i in range(6):
+    draw_q(rewardAvg[i])
+
+
+plt.legend(["epsilon0",'epsilon0.01','episilon0.1','tau1','tau0.1','random'])
+plt.title("average reward over time for 6 algorithms(run each algorithm %d times)"%trials)
+plt.xlabel("step")
+plt.ylabel("reward")
+plt.savefig("./fig2/reward_all2.png")
+
+plt.figure()
+for i in range(6):
+    draw_q(rewardStd[i])
+
+plt.legend(["epsilon0", 'epsilon0.01', 'episilon0.1', 'tau1', 'tau0.1', 'random'])
+plt.xlabel("step")
+plt.ylabel("reward")
+plt.title("reward std over time for 6 algorithms(run each algorithms %d times)" % trials)
+plt.savefig("./fig2/reawrd_std2.png")
+
+
+qstarAvg = []
+qstarStd = []
+
+for i in range(6):
+    agAvg = []
+    agStd = []
+    for action in range(4):
+        tAvg = []
+        tStd = []
+        for step in range(1000):
+            t = [agents[i][k].qStarAvg[action][step] for k in range(10)]
+            tAvg.append(np.mean(t))
+            tStd.append(np.std(t))
+        agAvg.append(tAvg)
+        agStd.append(tStd)
+    qstarAvg.append(agAvg)
+    qstarStd.append(agStd)
+
+plt.figure(figsize=(20,20))
+for i in range(4):
+    plt.subplot(2,2,i+1)
+    for j in range(6):
+        draw_q(qstarAvg[j][i])
+    plt.title("the average reward of action %d" %i)
+    plt.xlabel("step")
+    plt.ylabel("reward")
+    plt.legend(["epsilon0", 'epsilon0.01', 'episilon0.1', 'tau1', 'tau0.1', 'random'])
+plt.savefig("./fig2/actionAll2.png")
+
+plt.figure(figsize=(20,20))
+for i in range(4):
+    plt.subplot(2,2,i+1)
+    for j in range(6):
+        draw_q(qstarStd[j][i])
+    plt.title("the average std of action %d" %i)
+    plt.xlabel("step")
+    plt.ylabel("reward")
+    plt.legend(["epsilon0", 'epsilon0.01', 'episilon0.1', 'tau1', 'tau0.1', 'random'])
+plt.savefig("./fig2/actionStd2.png")
+
+plt.figure(figsize=(15,20))
+title= ['(epsilon=0)','(epsilon=0.01)','(epsilon=0.1)','(tau=1)','(tau=0.1)','(random)']
+for i in range(6):
+    plt.subplot(3,2,i+1)
+    action = [0 for i in range(1000*trials)]
+    for j in range(trials):
+        action[j*1000:(j+1)*1000]= agents[i][j].action[:]
+    plt.hist(action)
+    plt.title("histogram of actions selection (%d) trials "%trials+title[i])
+    plt.xlabel("step")
+    plt.ylabel("reward")
+plt.savefig("./fig2/hist2.png")
+
+
+# change parameters
+
+agents=[]
+for i in range(6):
+    temp = []
+    #play 10 times
+    for j in  range(trials):
+        temp.append(NArmQlearning(reward1))
+    agents.append(temp)
+
+# play 10 times and then avg the results
+for i in range(10):
+    agents[0][i].playepsilonT()
+    agents[1][i].playepsilon1()
+    agents[2][i].playepsilon2()
+    agents[3][i].playtau1()
+    agents[4][i].playtau2()
+    agents[5][i].playtauT()
+
+# avg the results
+rewardAvg = []
+rewardStd = []
+for i in range(6):
+    tavg=[]
+    tstd=[]
+    for j in range(1000):
+        t = [agents[i][k].rewardAvg[j] for k in range(trials)]
+        avg = np.mean(t)
+        tavg.append(avg)
+        tstd.append(np.std(t))
+
+    rewardAvg.append(tavg)
+    rewardStd.append(tstd)
+
+
+plt.figure()
+
+for i in range(6):
+    draw_q(rewardAvg[i])
+
+
+plt.legend(["time varing epsilon",'epsilon0.01','episilon0.1','tau1','tau0.1','time varing tau'])
+plt.title("average reward over time for 6 algorithms(run each algorithms %d times)"%trials)
+plt.xlabel("step")
+plt.ylabel("reward")
+plt.savefig("./fig3/reward_all3.png")
+
+plt.figure()
+for i in range(6):
+    draw_q(rewardStd[i])
+
+plt.legend(["time varing epsilon",'epsilon0.01','episilon0.1','tau1','tau0.1','time varing tau'])
+plt.title("reward std over time for 6 algorithms(run each algorithms %d times)" % trials)
+plt.xlabel("step")
+plt.ylabel("reward")
+plt.savefig("./fig3/reawrd_std3.png")
+
+
+qstarAvg = []
+qstarStd = []
+
+for i in range(6):
+    agAvg = []
+    agStd = []
+    for action in range(4):
+        tAvg = []
+        tStd = []
+        for step in range(1000):
+            t = [agents[i][k].qStarAvg[action][step] for k in range(10)]
+            tAvg.append(np.mean(t))
+            tStd.append(np.std(t))
+        agAvg.append(tAvg)
+        agStd.append(tStd)
+    qstarAvg.append(agAvg)
+    qstarStd.append(agStd)
+
+plt.figure(figsize=(20,20))
+for i in range(4):
+    plt.subplot(2,2,i+1)
+    for j in range(6):
+        draw_q(qstarAvg[j][i])
+    plt.title("the average reward of action %d" %i)
+    plt.legend(["time varing epsilon", 'epsilon0.01', 'episilon0.1', 'tau1', 'tau0.1', 'time varing tau'])
+    plt.xlabel("step")
+    plt.ylabel("reward")
+plt.savefig("./fig3/actionAll3.png")
+
+plt.figure(figsize=(20,20))
+for i in range(4):
+    plt.subplot(2,2,i+1)
+    for j in range(6):
+        draw_q(qstarStd[j][i])
+    plt.title("the average std of action %d" %i)
+    plt.legend(["time varing epsilon", 'epsilon0.01', 'episilon0.1', 'tau1', 'tau0.1', 'time varing tau'])
+    plt.xlabel("step")
+    plt.ylabel("reward")
+plt.savefig("./fig3/actionStd3.png")
+
+plt.figure(figsize=(15,20))
+plt.legend(["time varing epsilon",'epsilon0.01','episilon0.1','tau1','tau0.1','time varing tau'])
+for i in range(6):
+    plt.subplot(3,2,i+1)
+    action = [0 for i in range(1000*trials)]
+    for j in range(trials):
+        action[j*1000:(j+1)*1000]= agents[i][j].action[:]
+    plt.hist(action)
+    plt.title("histogram of actions selection (%d) trials "%trials+title[i])
+plt.savefig("./fig3/hist3.png")
 
